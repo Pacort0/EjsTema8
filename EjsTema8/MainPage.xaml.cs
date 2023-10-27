a185d34d929bdf12ba604f94290c1fda6d9345c2
siu <francisco.rodriguez@iesnervion.es>
2023-10-27 11:10:10 +0200

    Agregar archivos de proyecto.

---
+++ EjsTema8/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EjsTema8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            FrameExterno.WidthRequest = StackPadre.Width;
+            FrameExterno.HeightRequest = StackPadre.Height;
+        }
+    }
+}
